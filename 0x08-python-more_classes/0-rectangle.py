56ab4191f2fceb36b01f26abe2d273ecc8c6ca0e
CasualCartoon <muiruriian4@gmail.com>
2024-01-15 04:29:55 -0800

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
 class Rectangle:
+    """
+    Rectangle class defines a basic rectangle.
+
+    Attributes:
+        No attributes defined for an empty rectangle.
+
+    Methods:
+        No methods defined for an empty rectangle.
+
+    Example:
+        rect = Rectangle()
+    """
     pass
