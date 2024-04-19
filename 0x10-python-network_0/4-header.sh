45e1e0f8cdd44040f54214de65d515b03d4f0c77
CasualCartoon <muiruriian4@gmail.com>
2024-04-19 11:02:47 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response, A header variable X-HolbertonSchool-User-Id must be sent with the value 98
+# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
+# A header variable X-HolbertonSchool-User-Id must be sent with the value 98
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a GET request with a custom header and display the response body
 curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
